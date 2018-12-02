cb59880a532edbe9bd1d80008d6ad1a358ed60b0
Tillmann Heigel <tillmann.heigel@idealo.de>
2018-12-02 17:54:11 +0100

    aoc-2018: 1 restructuring

--- src/de/tillmannheigel/advent_2018/Day_1/A.java
+++ src/de/tillmannheigel/advent_2018/Day_1/A.java
@@ -1,15 +1,15 @@
-package de.tillmannheigel.advent_2018;
+package de.tillmannheigel.advent_2018.Day_1;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Main {
+public class A {
 
     private static int sum = 0;
 
     public static void main(String[] args) throws IOException {
-        Files.lines(Paths.get("src/de/tillmannheigel/advent_2018/res.file")).forEach(Main::add);
+        Files.lines(Paths.get("src/de/tillmannheigel/advent_2018/Day_1/res.file")).forEach(A::add);
         System.out.println(sum);
     }
 
@@ -17,8 +17,8 @@ public class Main {
         System.out.print(sum + " " + line);
         char operation = line.toCharArray()[0];
         Integer value = Integer.valueOf(line.substring(1));
-        if (operation == '+') sum = sum + value;
-        if (operation == '-') sum = sum - value;
+        if (operation == '+') sum += value;
+        if (operation == '-') sum -= value;
         System.out.println(" = " + sum);
     }
 }
