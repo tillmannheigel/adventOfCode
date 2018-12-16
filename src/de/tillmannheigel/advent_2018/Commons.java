5e1eccb6cce5b7fd2c0e3e124b5b68a836ee561f
Tillmann Heigel <heigel@tillmannheigel.de>
2018-12-16 17:27:55 +0100

    Day05: B struggling ;)

--- src/de/tillmannheigel/advent_2018/Commons.java
+++ src/de/tillmannheigel/advent_2018/Commons.java
@@ -1,5 +1,10 @@
 package de.tillmannheigel.advent_2018;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -24,5 +29,15 @@ public class Commons {
         if (operation == '+') return value;
         if (operation == '-') return (-1) * value;
         return 0;
+    }
+
+    public static ArrayList<Character> readCharacters(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+        ArrayList<Character> characters = new ArrayList<>();
+        int current;
+        while ((current = reader.read()) != -1) {
+            characters.add((char) current);
+        }
+        return characters;
     }
 }
---
+++ src/de/tillmannheigel/advent_2018/Day_5_Alchemical_Reduction/A.java
@@ -0,0 +1,55 @@
+package de.tillmannheigel.advent_2018.Day_5_Alchemical_Reduction;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+import static de.tillmannheigel.advent_2018.Commons.readCharacters;
+
+public class A {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<Character> characters = readCharacters("src/de/tillmannheigel/advent_2018/Day_5_Alchemical_Reduction/input");
+
+        int reductionResult = recursiveReduction(characters);
+
+        System.out.println(reductionResult);
+    }
+
+    private static int recursiveReduction(List<Character> characters) {
+        List<Character> reducedCharacters = reduce(characters);
+        return (characters.size() == reducedCharacters.size()) ? reducedCharacters.size() : recursiveReduction(reducedCharacters);
+    }
+
+    private static List<Character> reduce(List<Character> characters) {
+        ListIterator<Character> listIterator = characters.listIterator();
+        List<Character> result = new ArrayList<>();
+        while (listIterator.hasNext()) {
+            Character next = listIterator.next();
+            if (!result.isEmpty()) {
+                Character last = result.get(result.size() - 1);
+                if(reduce(next, last)){
+                    result.remove(last);
+                } else {
+                    result.add(next);
+                }
+            } else {
+                result.add(next);
+            }
+        }
+        return result;
+    }
+
+    private static boolean reduce(char next, char last) {
+        return isSameLetter(next,last) && isDifferentCase(next,last);
+    }
+
+    private static boolean isSameLetter(char next, char last) {
+        return Character.toLowerCase(next) == Character.toLowerCase(last);
+    }
+
+    private static boolean isDifferentCase(char next, char last) {
+        return Character.isUpperCase(next) == Character.isLowerCase(last);
+    }
+}
